f90a6e6265ba641a6d71e487c1b8d7858d115feb
Toader Carausu <tcarausu@gmail.com>
2020-05-15 17:20:48 +0300

    setup_for_lab7/8

---
+++ src/main/java/lab8/GL_Operations_Lab8_Ex.java
@@ -0,0 +1,58 @@
+package lab8;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.awt.GLCanvas;
+import javax.swing.*;
+import java.io.FileNotFoundException;
+
+public class GL_Operations_Lab8_Ex implements GLEventListener {
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        final GLProfile profile = GLProfile.get(GLProfile.GL2);
+        GLCapabilities capabilities = new GLCapabilities(profile);
+        // The canvas
+        final GLCanvas glcanvas = new GLCanvas(capabilities);
+        GL_Operations_Lab8_Ex l = new GL_Operations_Lab8_Ex();
+
+        glcanvas.addGLEventListener(l);
+        glcanvas.setSize(700, 700);
+
+
+        final JFrame frame = new JFrame("lab8");
+        //adding canvas to frame
+        frame.getContentPane().add(glcanvas);
+        frame.setSize(frame.getContentPane().getPreferredSize());
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        //mandle fractile
+    }
+
+}
+
